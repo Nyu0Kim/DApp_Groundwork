836be4cb8e87e3bc85c4d1c1f7138757714ea51c
Nyu0Kim <nyu0kim@gmail.com>
2024-02-10 14:19:36 +0900

    Add: form - ex-contract sol file

---
+++ example/ex-contract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+
+// create a contract that can store data and return the data back
+
+// be able to do the following:
+
+// 1. recieve information, 2. store information, 3. return the information back
+
+// A contract in the sense of Solidity is a collection of code(its functions) and data (its state) that resides at a specific address on the Ethereum BlockChain
+
+contract simpleStorage {
+    // write all the code inside here - functions and its state
+
+    uint storeData
+}
