010ac979f57f32baf4454b8faf9d4e453b867440
Nyu0Kim <nyu0kim@gmail.com>
2024-02-11 09:58:32 +0900

    Edit: Repository Readme

---
+++ example/deploy-smart-contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+// TODO:
+// 1. Create a storage contract that sets and gets values - Only the value it returns is multipled 5x
+// 2. Create a storage contract that sets and gets values of NAMES
+
+
